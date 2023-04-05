ecf6d9aca4be2422d745fa043e1d533fe9baf126
8baller <8ball030@gmail.com>
2023-04-06 00:40:08 +0200

    [admin] disabled debug mode

--- services/backend/app/settings.py
+++ services/backend/app/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n32=(0=+7*@j54jga!%%5&_a4b^9pqht=%sa+5wo^c$k4z6h5%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition

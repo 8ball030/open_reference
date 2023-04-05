941bc66a74f8b8c7f27525017b073a30f43fccbe
8baller <8ball030@gmail.com>
2023-04-06 00:22:10 +0200

    [feat] initial model migrations

--- services/backend/repos/models.py
+++ services/backend/repos/models.py
@@ -1,3 +1,22 @@
+"""Definition of the initial models."""
+
 from django.db import models
 
-# Create your models here.
+class Owner(models.Model):
+    handle = models.CharField(max_length=100)
+
+class Repository(models.Model):
+    owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+
+class Commit(models.Model):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    hash = models.CharField(max_length=100)
+    message = models.TextField()
+
+class PullRequest(models.Model):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    author = models.CharField(max_length=100)
+

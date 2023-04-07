f1253ba6a82ff2a73dcbe21189a55243ee722d2b
8baller <tomrae030@hotmail.com>
2023-04-07 13:59:48 +0100

    [feat] added in tests

---
+++ services/backend/repos/constants.py
@@ -0,0 +1,14 @@
+DEFAULT_AUTHOR = "8ball030"
+DEFAULT_OWNER = "8ball030"
+DEFAULT_REPO_NAME = "ALICE"
+DEFAULT_REPO_URL = "https://github.com/8ball030/open_reference"
+
+DEFAULT_TOOL = "Command Line"
+DEFAULT_TOOL_URL = "https://www.gnu.org/software/bash"
+DEFAULT_TOOL_DESCRIPTION = "The command line is a text interface for your computer. It's a program that takes in commands, which it passes on to the computer's operating system to run."
+DEFAULT_LANGUAGE = "Python"
+DEFAULT_LANGUAGE_URL = "https://www.python.org"
+DEFAULT_LANGUAGE_DESCRIPTION = "Python is a programming language that lets you work quickly and integrate systems more effectively."
+
+DEFAULT_ROLE = "Devops"
+DEFAULT_ROLE_DESCRIPTION = "DevOps is a set of practices that combines software development (Dev) and information-technology operations (Ops). It aims to shorten the systems development life cycle and provide continuous delivery with high software quality."
--- services/backend/tests/test_urls.py
+++ services/backend/tests/test_urls.py
@@ -3,10 +3,29 @@ Simple tests for the django backend.
 """
 from django.test import TestCase
 
-class BaseTest(TestCase):
 
+class BaseTest(TestCase):
     def test_base_admin_page(self):
         """Checks if the base admin page returns a 301."""
-        response = self.client.get('/admin')
-        self.assertEqual(response.status_code, 301)
+        response = self.client.get("/admin")
+        self.assertEqual(response.status_code, 302)
 
+    def test_renders_tools_page(self):
+        """Checks if the tools page renders."""
+        response = self.client.get("/tools")
+        self.assertEqual(response.status_code, 200)
+
+    def test_renders_roles_page(self):
+        """Checks if the roles page renders."""
+        response = self.client.get("/roles")
+        self.assertEqual(response.status_code, 200)
+
+    def test_renders_repos_page(self):
+        """Checks if the repos page renders."""
+        response = self.client.get("/repos")
+        self.assertEqual(response.status_code, 200)
+
+    def test_renders_homepage(self):
+        """Checks if the repos page renders."""
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)

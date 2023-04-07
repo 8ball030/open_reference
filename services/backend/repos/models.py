76d9a186523df6928b3158c1b774fa83959c9f6d
8baller <tomrae030@hotmail.com>
2023-04-07 13:58:42 +0100

    [feat] added in models

--- services/backend/repos/models.py
+++ services/backend/repos/models.py
@@ -1,18 +1,33 @@
 """Definition of the initial models."""
+from datetime import datetime
 
 from django.db import models
 
+
+class Author(models.Model):
+    handle = models.CharField(max_length=100, unique=True, null=False, blank=False)
+
+
 class Owner(models.Model):
-    handle = models.CharField(max_length=100)
+    handle = models.CharField(max_length=100, unique=True, null=False, blank=False)
+
 
 class Repository(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    url = models.URLField(null=False, blank=False)
+    last_modified = models.DateTimeField(default=None, null=True)
+    last_checked = models.DateTimeField(default=None, null=True)
+
 
 class Commit(models.Model):
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
     hash = models.CharField(max_length=100)
     message = models.TextField()
+    date = models.DateTimeField(default=datetime.now)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
 
 class PullRequest(models.Model):
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
@@ -20,3 +35,21 @@ class PullRequest(models.Model):
     body = models.TextField()
     author = models.CharField(max_length=100)
 
+
+class Language(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField(null=False, blank=False)
+
+
+class Tool(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.URLField(null=False, blank=False)
+    description = models.TextField()
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    start_date = models.DateTimeField(default=None, null=True)
+    end_date = models.DateTimeField(default=None, null=True)
